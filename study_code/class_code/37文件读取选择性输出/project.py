0e02b8063fafa86bea4ee2dba8457f75fd62e856
Silence <taoranzhishang@hotmail.com>
2020-11-29 22:07:24 +0800

    新增

--- study_code/class_code/37文件读取选择性输出/project.py
+++ study_code/class_code/37文件读取选择性输出/project.py
@@ -1,9 +1,14 @@
+'''
+读取文件
+识别首字符不是#的行并打印
+'''
+
 file = open("file", 'r',encoding='utf-8')
 counter = 1
 
 for line in file:
     if line[0] != '#':
-        print("第%d行" % (counter), line)
+        print("第%d行：" % (counter), line)
         counter += 1
     else:
         pass

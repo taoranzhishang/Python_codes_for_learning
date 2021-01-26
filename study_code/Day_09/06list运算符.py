d9ed8a2e9ec26d534465292b73b928e03acf6c4f
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-26 12:36:51 +0800

    update

--- study_code/Day_09/06list运算符.py
+++ study_code/Day_09/06list运算符.py
@@ -3,7 +3,7 @@ mylist2 = [6, 6, 6, 1, 22, 33, 44, 55, 66, 12]
 print(1 in mylist1)  # 判读存在
 print(1 not in mylist2)  # 判断不存在
 print(mylist1 + mylist2)  # 拼接，两个和为一个；归并是去除重复的
-print(mylist1 * 4)  # 连续复制4遍，四个为一个list
+print(mylist1 * 4)  # 连续复制4遍，四个为一个list，维度不变，体型翻倍
 print(len(mylist1), len(mylist2))  # 求长度
 print(max(mylist1))
 print(min(mylist2))

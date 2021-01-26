fd055e6e5527e59304e89393fad7a2a6237287cf
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-26 11:39:45 +0800

    update

--- study_code/Day_08/11字符串编码注意事项.py
+++ study_code/Day_08/11字符串编码注意事项.py
@@ -7,10 +7,10 @@ print(b)
 c = bytes("你好天朝", "gbk")  # gbk编码格式
 print(c)
 # b'  bytes类型
-print(bytes("我", "utf-8"))  # b'\xe6\x88\x91'  两个字符表示一个汉字，一个汉字结束符
-print(bytes("我的", "utf-8"))  # b'\xe6\x88\x91\xe7\x9a\x84'  两个汉字4字符，一个结束符
-print(bytes("我的啊", "utf-8"))  # b'\xe6\x88\x91\xe7\x9a\x84\xe5\x95\x8a'  3个汉字6字符，一个结束符
+print(bytes("我", "utf-8"))  # b'\xe6\x88\x91'utf-8一个汉字3字节
+print(bytes("我的", "utf-8"))  # b'\xe6\x88\x91\xe7\x9a\x84'
+print(bytes("我的啊", "utf-8"))  # b'\xe6\x88\x91\xe7\x9a\x84\xe5\x95\x8a'
 
-print(bytes("我", "gbk"))  # b'\xce\xd2'  一个汉字两个字符，没有结束符
-print(bytes("我的", "gbk"))  # b'\xce\xd2\xb5\xc4'  两个汉字4个字符，没有结束符
-print(bytes("我的啊", "gbk"))  # b'\xce\xd2\xb5\xc4\xb0\xa1'  三个汉字6个字符，没有结束符
+print(bytes("我", "gbk"))  # b'\xce\xd2'gbk一个汉字两个字符
+print(bytes("我的", "gbk"))  # b'\xce\xd2\xb5\xc4'
+print(bytes("我的啊", "gbk"))  # b'\xce\xd2\xb5\xc4\xb0\xa1'

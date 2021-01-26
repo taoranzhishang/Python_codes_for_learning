a9a545e3473496b117c86b54e47d96e9f1e3fafa
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-26 12:06:15 +0800

    update

--- study_code/Day_08/28startwith.py
+++ study_code/Day_08/28startwith.py
@@ -1,7 +1,10 @@
-str = "hello world"
+str = "heLLo world"
 print(str.startswith('he'))  # 判断以指定字符字符串开头，返回True或false
+"""
+对字符串中的大小写字母互换，大换小，小换大
+"""
 print(str.swapcase())  # 字符串小写换大写,不改变原字符串大小写
 print(str)
-#print(str.swapcase())连续转换，结果不变
+# print(str.swapcase())连续转换，结果不变
 str1 = str.swapcase()
 print(str1.swapcase())  # 字符串大写换小写

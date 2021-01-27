8c37ec263223536046cae00eb580581f10dca932
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-27 12:04:18 +0800

    update

--- study_code/Day_11/04输入输出/11文件写入英文.py
+++ study_code/Day_11/04输入输出/11文件写入英文.py
@@ -4,6 +4,7 @@
 # @File:    11文件写入英文.py
 # @Time:    2020/12/13 1:37
 # @Software:PyCharm
+
 file = open(r"D:\code\py\study_code\Day_11\04输入输出\demo.txt", 'w')  # 以写的方式打开demo.txt,没有则新建
 myStr = "Hello Python"
 file.write(myStr)  # 往demo.txt中写入myStr，英文以ASCII自动编码

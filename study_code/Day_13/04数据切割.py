7c73a1d9e5bd9df0eb5ec4e43ff9aeffbe6f9906
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-27 18:27:55 +0800

    数据处理

--- study_code/Day_13/04数据切割.py
+++ study_code/Day_13/04数据切割.py
@@ -1,13 +1,18 @@
 # import os
 # os.mkdir(r"C:\Users\Tsinghua-yincheng\Desktop\day13\split") #创建文件夹
 
+"""
+创建切割小文件
+"""
 num = 10
 filesplitlist = []  # 文件集合
 for i in range(num):
     filepath = r"C:\Users\Tsinghua-yincheng\Desktop\day13\split\data" + str(i + 1) + ".txt"
     file = open(filepath, "wb")
     filesplitlist.append(file)
-
+"""
+读取大文件
+"""
 fileallpath = r"C:\Users\Tsinghua-yincheng\Desktop\day13\bigdata.txt"  # 归并
 fileall = open(fileallpath, "rb")  # 写入
 i = 0

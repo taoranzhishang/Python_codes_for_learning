d1b05f491273f03098cc6034f618183fd4d3fb29
taoranzhishang <taoranzhishang@hotmail.com>
2021-01-29 21:13:50 +0800

    重载运算符

--- study_code/Day_14/19重载运算符的返回值.py
+++ study_code/Day_14/19重载运算符的返回值.py
@@ -11,7 +11,7 @@ class complex:
         重载：针对本类型对+重新解释一种行为
         """
 
-        return complex(self.x + other.x, self.y + other.y)  # 加法的返回值
+        return complex(self.x + other.x, self.y + other.y)  # 加法的返回值，一个匿名对象
 
 
 cl1 = complex(1, 2)
@@ -19,5 +19,5 @@ cl2 = complex(3, 4)
 cl1.show()
 cl2.show()
 # cl3 = cl1 + cl2  # 与下一行等价
-cl3 = cl1.__add__(cl2)
+cl3 = cl1.__add__(cl2)  # 返回一个匿名对象，赋值给cl3，等价于实例化，cl3就可以调用方法
 cl3.show()

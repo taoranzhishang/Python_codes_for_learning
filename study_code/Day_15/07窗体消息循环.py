a003fddecddcf31acf77ab01fe78046f7ab8d6ea
taoranzhishang <taoranzhishang@hotmail.com>
2021-02-01 16:56:33 +0800

    update

--- study_code/Day_15/07窗体消息循环.py
+++ study_code/Day_15/07窗体消息循环.py
@@ -1,4 +1,4 @@
 import tkinter
 
-win=tkinter.Tk()#tkinter是一个模块，Tk()是构造函数
-win.mainloop()#进入消息循环，windows创建一个窗体就会就入消息循环，由系统发送命令
+win = tkinter.Tk()  # tkinter是一个模块，Tk()是构造函数
+win.mainloop()  # 进入消息循环，windows创建一个窗体就会就入消息循环，由系统发送命令

43ff84ca2346843c02718325eb36ad3c19a31603
taoranzhishang <taoranzhishang@hotmail.com>
2021-02-01 16:03:18 +0800

    update

--- study_code/Day_14/20重载运算符多个类型.py
+++ study_code/Day_14/20重载运算符多个类型.py
@@ -26,10 +26,10 @@ cl1 = complex(1, 2)
 cl2 = complex(3, 4)
 cl1.show()
 cl2.show()
-# cl3=cl1.__add__(10)
 # cl3=cl1.__add__(cl2)
 # cl3 = cl1 + cl2  # 不能分辨使用哪个add，不能解释
 # cl3.show()
+# cl4=cl1.__add__(10)
 # cl4 = cl1 + 10  # 只用了第二个add,但是可以运行,可以重新解释
 # cl4.show()
 

b79af66bbe6e86b8dbc3174aca147e0f028b522a
taoranzhishang <taoranzhishang@hotmail.com>
2021-02-03 16:03:24 +0800

    异常处理

--- study_code/Day_16/10异常处理继续执行.py
+++ study_code/Day_16/10异常处理继续执行.py
@@ -1,7 +1,7 @@
 num1 = eval(input("num1:"))
 num2 = eval(input("num2:"))
 try:  # 尝试
-    print(num1 / num2)  # ZeroDivisionError: division by zero
-except ZeroDivisionError:  # 处理错误
+    print(num1 / num2)  # ZeroDivisionError: division by zero，正常情况下执行
+except ZeroDivisionError:  # 处理错误，错误情况下执行
     print("num2不可以为0")
-print("一定要记住哦")
+print("一定要记住哦")  # 不管有无错误都会执行

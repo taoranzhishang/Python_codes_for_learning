a4ae769081943ec799b6d30fa195cb3f4df4df96
taoranzhishang <taoranzhishang@hotmail.com>
2021-02-08 18:52:20 +0800

    栈与队列遍历文件

--- study_code/Day_19/深度遍历与广度遍历/11队列广度遍历.py
+++ study_code/Day_19/深度遍历与广度遍历/11队列广度遍历.py
@@ -1,17 +1,17 @@
-import  os
-from  collections  import deque
+import os
+from collections import deque
 
-path=r"C:\Users\Tsinghua-yincheng\Desktop\day19"
-queue=deque([]) #队列
+path = r"C:\Users\Tsinghua-yincheng\Desktop\day19"
+queue = deque([])  # 队列
 queue.append(path)
-while len(queue)!=0:
-    path=queue.popleft()#取出拉出的值
-    filelist=os.listdir(path) #遍历路径
-    for  filename  in filelist:
-        filepath=os.path.join(path,filename)
+while len(queue) != 0:
+    path = queue.popleft()  # 取出拉出的值
+    filelist = os.listdir(path)  # 遍历路径
+    for filename in filelist:
+        filepath = os.path.join(path, filename)
 
         if os.path.isdir(filepath):
-            print("文件夹",filename)
+            print("文件夹", filename)
             queue.append(filepath)
         else:
             print("文件", filename)

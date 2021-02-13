8fd66c9e22f3974643febad1e20891ed1a7dc890
taoranzhishang <taoranzhishang@hotmail.com>
2021-02-13 16:45:55 +0800

    线程的执行与速度

---
+++ study_code/Day_22/05多线程的速度.py
@@ -0,0 +1,18 @@
+import _thread  # 多线程
+import time
+
+
+def go():
+    for i in range(10):  # 10秒
+        print(i, "---------")
+        time.sleep(1)
+
+
+# for  i  in range(5): #50秒
+#   go()
+for i in range(5):
+    _thread.start_new_thread(go, ())
+
+for i in range(10):  # 10秒
+    time.sleep(1)
+print("game over")
